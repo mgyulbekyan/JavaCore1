1cbfd0455fae078724d7d24b5b3cfbb81ef0ae89
Mariam <gyulbekyanm94@mail.ru>
2021-10-09 12:08:14 +0400

    add java file

---
+++ src/lesson4/Array.java
@@ -0,0 +1,22 @@
+package lesson4;
+
+public class Array {
+    public static void main(String[] args) {
+        int monthDay[];
+        monthDay = new int[12];
+        monthDay[0] = 31;
+        monthDay[1] = 28;
+        monthDay[2] = 31;
+        monthDay[3] = 30;
+        monthDay[4] = 31;
+        monthDay[5] = 30;
+        monthDay[6] = 31;
+        monthDay[7] = 31;
+        monthDay[8] = 30;
+        monthDay[9] = 31;
+        monthDay[10] = 30;
+        monthDay[11] = 31;
+        System.out.println("Մայիս ամիսն ունի "  +  monthDay[4]  +  " օր։");
+
+    }
+}
