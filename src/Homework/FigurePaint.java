834b8b65114d7d32b1afc4a461bdeb555aefd90b
Mariam <gyulbekyanm94@mail.ru>
2021-10-10 10:31:40 +0400

    add java file

--- src/Homework/FigurePaint.java
+++ src/Homework/FigurePaint.java
@@ -3,19 +3,83 @@ package Homework;
 public class FigurePaint {
     public static void main(String[] args) {
         for (int i = 0; i < 4; i++) {
-            for (int j = 0; j <=i; j++) {
+            for (int j = 0; j <= i; j++) {
                 System.out.print("* ");
 
             }
             System.out.println("");
 
+
+
+            }
+        for (int i = 0; i <4; i++) {
+            for (int j =4; j>i; j--) {
+                System.out.print("  ");}
+            for (int j = 0; j <=i; j++) {
+                System.out.print(" *");
+
+            }
+            System.out.println();
+
+
+                }
+
+        for (int i = 0; i <4; i++) {
+            for (int j =4; j>i; j--) {
+                System.out.print("* ");}
+            for (int j =0; j <=i; j++) {
+                System.out.print(" ");
+
+
+            }
+            System.out.println();
+
         }
 
 
-        for (int i = 0; i < 4; i++) {
-            for (int j = 4; j >=i; j--) {
+        for (int i = 4; i >0; i--) {
+            for (int j =4; j>i; j--) {
+                System.out.print("  ");
 
-                System.out.println("*");
+            }
+            for (int j = 0; j<i; j++) {
+                System.out.print("* ");
+
+            }
+            System.out.println();
+
+        }
+        for (int i = 0; i <4; i++) {
+            for (int j = 4; j>i; j--) {
+                System.out.print(" ");
+
+            }
+            for (int j = 0; j <=i; j++) {
+                System.out.print("* ");
+
+            }
+            System.out.println();
+
+        }
+        for (int i =3; i>0; i--) {
+            System.out.print(" ");
+            for (int j=i; j<3; j++) {
+                System.out.print(" ");
+
+
+                    }
+            for (int j =0; j<i; j++) {
+                System.out.print(" *");
+
+            }
+            System.out.println();
+
+
+                }
+         {
+
+        }
+
             }
 
 
@@ -23,5 +87,29 @@ public class FigurePaint {
 
 
 
-    }
-}
+
+
+
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
