df88fa79d7561c78e99ce1e38fc116bcc25674ee
Mariam <gyulbekyanm94@mail.ru>
2021-10-15 22:54:17 +0400

    add java file

---
+++ src/arrayutil/ArrayUtil.java
@@ -0,0 +1,73 @@
+package arrayutil;
+
+public class ArrayUtil {
+    public static void main(String[] args) {
+        int[] array = {25, 50, 85, 10, 35, 20, 15, 60, 45, 95};
+        //Տպել մասիվի բոլոր էլեմենտները
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+        //Տպել մասիվի բոլոր զույգ էլեմենտները
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                System.out.println(array[i]);
+            }
+        }
+        //Տպել մասիվի բոլոր կենտ էլեմենտները
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 1) {
+                System.out.println(array[i]);
+            }
+
+
+        }
+        //Տպել մասիվի ամենափոքր էլեմենտը
+        int min=array[0];
+        for (int i = 0; i < array.length; i++)
+        { if(array[i]<min)
+            {System.out.println(min);}
+            }
+
+//Տպել մասիվի էլեմենտների միջի թվաբանականը
+            int num = array[0];
+            for (int i = 0; i < array.length; i++)
+            {  num += array[i];
+
+            System.out.println(num / array.length);}
+        }
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
