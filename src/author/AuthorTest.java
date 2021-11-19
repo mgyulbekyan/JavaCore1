227a3451dc366e28615b69ccd51c9516554b3766
Mariam <gyulbekyanm94@mail.ru>
2021-11-19 19:17:45 +0400

    add java file

---
+++ src/author/AuthorTest.java
@@ -0,0 +1,37 @@
+package author;
+
+import java.util.Scanner;
+
+import author.Author;
+
+public class AuthorTest {
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        AuthorStorage authorStorage = new AuthorStorage();
+        Author author = new Author(n)
+        System.out.println("How many authors we have?");
+        int a = scanner.nextInt();
+
+
+        for (int i = 0; i < a; i++) {
+            System.out.println("name");
+            String name = scanner.next();
+            System.out.println("surname");
+            String surname = scanner.next();
+            System.out.println("email");
+            String email = scanner.next();
+            System.out.println("age");
+            int age = scanner.nextInt();
+            System.out.println("gender");
+            String gender = scanner.next();
+
+
+        }
+        System.out.println(authorStorage.print());
+
+
+    }
+
+}
